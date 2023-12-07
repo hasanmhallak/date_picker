b3f624ce59ec4c98cd9dba1e14c3a999ba3e4ed4
Hasan M. Hallak <hasanm.hallak@gmail.com>
2023-12-07 13:39:53 +0200

    add docs to header

--- lib/src/shared/header.dart
+++ lib/src/shared/header.dart
@@ -3,25 +3,46 @@ import 'package:flutter/material.dart';
 import 'leading_date.dart';
 import 'page_sliders.dart';
 
-/// The header that will be shown above the calendar grid.
+/// The `Header` class represents the header widget that is displayed above the calendar grid.
 ///
+/// This widget includes information about the currently displayed date, as well as navigation controls
+/// for moving to the next or previous page in the calendar. It consists of a [LeadingDate] widget,
+/// which indicates the current year and month, and a [PageSliders] widget for navigating between pages.
+///
+/// ### Example:
+///
+/// ```dart
+/// Header(
+///   displayedDate: "December 2023",
+///   onDateTap: () {
+///     // Handle date tap action
+///   },
+///   onNextPage: () {
+///     // Handle next page navigation
+///   },
+///   onPreviousPage: () {
+///     // Handle previous page navigation
+///   },
+///   slidersColor: Colors.blue,
+///   slidersSize: 20.0,
+///   leadingDateTextStyle: TextStyle(
+///     fontWeight: FontWeight.bold,
+///     fontSize: 18.0,
+///   ),
+/// )
+/// ```
 ///
 /// See also:
 ///
-///  * [LeadingDate], which shows indication about what year/month are opened.
+///  * [LeadingDate], a widget that shows an indication of the opened year/month.
 ///
-/// * [PageSliders], which controlls the next & previous page.
+///  * [PageSliders], a widget that controls the next and previous page navigation.
 ///
 class Header extends StatelessWidget {
-  final String displayedDate;
-  final VoidCallback onDateTap;
-  final VoidCallback onNextPage;
-  final VoidCallback onPreviousPage;
-  final Color slidersColor;
-  final double slidersSize;
-  final TextStyle leadingDateTextStyle;
-
-  /// The header that will be shown above the calendar grid.
+  /// Creates a new [Header] instance.
+  ///
+  /// The [displayedDate], [onDateTap], [onNextPage], [onPreviousPage], [slidersColor],
+  /// [slidersSize], and [leadingDateTextStyle] parameters are required.
   const Header({
     super.key,
     required this.displayedDate,
@@ -32,6 +53,36 @@ class Header extends StatelessWidget {
     required this.slidersSize,
     required this.leadingDateTextStyle,
   });
+
+  /// The currently displayed date. It is typically in a format
+  /// indicating the month and year.
+  final String displayedDate;
+
+  /// the text style for the [displayedDate] in the header.
+  final TextStyle leadingDateTextStyle;
+
+  /// Called when the displayed date is tapped. This can
+  /// be used to trigger actions related to selecting or
+  /// interacting with the displayed date.
+  final VoidCallback onDateTap;
+
+  /// called when the user wants to navigate to the next
+  /// page in the calendar. This function is associated
+  /// with the forward navigation control.
+  final VoidCallback onNextPage;
+
+  /// called when the user wants to navigate to the
+  /// previous page in the calendar. This function is
+  /// associated with the backward navigation control.
+  final VoidCallback onPreviousPage;
+
+  /// The color of the page navigation sliders
+  /// (forward and backward).
+  final Color slidersColor;
+
+  /// The size of the page navigation sliders
+  /// (forward and backward).
+  final double slidersSize;
 
   @override
   Widget build(BuildContext context) {
